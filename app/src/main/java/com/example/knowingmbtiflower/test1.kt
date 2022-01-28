4b23fdfcd9d3e0ec732ce811dce7d6ea10aab0b7
moonsoo20 <skql3398@naver.com>
2022-01-28 20:56:25 +0900

    변경

--- app/src/main/java/com/example/knowingmbtiflower/test1.kt
+++ app/src/main/java/com/example/knowingmbtiflower/test1.kt
@@ -3,9 +3,9 @@ package com.example.knowingmbtiflower
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TestActivity : AppCompatActivity() {
+class test1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`activity_test1.xml`)
+        setContentView(R.layout.activity_test13)
     }
 }
